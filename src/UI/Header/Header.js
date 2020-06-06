3aa959c4291b0bb57b30caa330f7e013f7ed4831
Rashad Ataf <rashadattaf@gmail.com>
2020-06-06 15:40:53 +0300

    fixing: bug in the code

--- src/UI/Header/Header.js
+++ src/UI/Header/Header.js
@@ -2,7 +2,6 @@ import React from "react";
 import classes from './Header.module.css';
 import NavigationBar from "../../Components/Navigation/NavigationBar";
 import {Route} from "react-router";
-import List from "../List/List";
 
 // the Header component represents the
 // header of our page
@@ -15,8 +14,6 @@ const Header = (props) => {
             <Route path="/" exact render={() => {
                 return <p className={classes.HeaderHero}>current movie</p>
             }} />
-            {/* draw the list of search pages only if the url /search/{something} */}
-            <Route path="/search/:search/:num" exact render={() => <List numOfPages={5} />} />
         </header>
     );
 }
