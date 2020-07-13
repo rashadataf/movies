57d47c4229610bb0a8f496e91729095e49d2cd6b
Rashad Ataf <rashadattaf@gmail.com>
2020-07-13 23:58:40 +0300

    Updates

---
+++ precache-manifest.c4aa2605960e84d3b64cbc212f09e611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1599566efdd21cb3a0c056860e3a7efe",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "8e0070e1ccf381effe30",
+    "url": "/movies/static/css/main.25003076.chunk.css"
+  },
+  {
+    "revision": "c76e8a32ac176cf8207f",
+    "url": "/movies/static/js/2.bcf64849.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies/static/js/2.bcf64849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e0070e1ccf381effe30",
+    "url": "/movies/static/js/main.4dded42c.chunk.js"
+  },
+  {
+    "revision": "01c2ea6e27a1b990f0f2",
+    "url": "/movies/static/js/runtime-main.0c12c8de.js"
+  }
+]);
